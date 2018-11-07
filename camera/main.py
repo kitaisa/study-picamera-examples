5a8aad1def2e2eaa45d9b71d2a60e0bc207e491b
kitaisa <44741354+kitaisa@users.noreply.github.com>
2018-11-08 00:27:02 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, Response
-from processor.simple_streamer import SimpleStreamer as VideoCamera
+#from processor.simple_streamer import SimpleStreamer as VideoCamera
 # from processor.pedestrian_detector import PedestrianDetector as VideoCamera
-# from processor.motion_detector import MotionDetector as VideoCamera
+from processor.motion_detector import MotionDetector as VideoCamera
 # from processor.qr_detector import QRDetector as VideoCamera
 # from processor.face_detector import FaceDetector as VideoCamera
 # from processor.person_detector import PersonDetector as VideoCamera
@@ -28,6 +28,6 @@ def gen(camera):
 def video_feed():
     return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-#
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, threaded=True)

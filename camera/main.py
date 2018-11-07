bd66608f000d0d7fc4ca3de8ef86c70e33903348
kitaisa <44741354+kitaisa@users.noreply.github.com>
2018-11-07 23:44:53 +0900

    Update main.py

--- camera/main.py
+++ camera/main.py
@@ -28,6 +28,6 @@ def gen(camera):
 def video_feed():
     return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
+#
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=False, threaded=True)
